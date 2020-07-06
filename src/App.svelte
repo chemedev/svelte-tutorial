31f439e62dc9a73a5316f2fa96ef28b3241790ff
Juan Chemello <juanchemell@gmail.com>
2020-07-05 21:34:33 -0300

    loops-events-conditionals

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,16 @@
 <script>
-  let firstName = 'Jimi';
-  let lastName = 'Hendrix';
-  let beltColour = 'black';
+  let people = [
+    { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+    { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+    { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+  ];
 
-  $: fullName = `${firstName} ${lastName}`;
-  $: {
-    console.log(beltColour);
-    console.log(fullName);
-  }
+  const handleClick = (e, id) => {
+    people = people.filter(person => person.id !== id);
+    console.log(e);
+  };
 
-  const handleClick = () => {
-    beltColour = 'orange';
-  };
-  const handleInput = e => {
-    beltColour = e.target.value;
-  };
+  let num = 3;
 </script>
 
 <style>
@@ -24,7 +20,6 @@
     max-width: 240px;
     margin: 0 auto;
   }
-
   @media (min-width: 640px) {
     main {
       max-width: none;
@@ -32,9 +27,27 @@
   }
 </style>
 
+{#if num > 20}
+  <p>Greater than 20</p>
+{:else if num > 5}
+  <p>Greater than 5</p>
+{:else}
+  <p>Not greater than 5</p>
+{/if}
+
 <main>
-  <p style="color: {beltColour}">{fullName} - {beltColour} belt</p>
-  <input type="text" bind:value={firstName} />
-  <input type="text" bind:value={lastName} />
-  <input type="text" bind:value={beltColour} />
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      {#if person.beltColour === 'black'}
+        <p>
+          <strong>MASTER NINJA</strong>
+        </p>
+      {/if}
+      <p>{person.age} years old, {person.beltColour} belt</p>
+      <button on:click={e => handleClick(e, person.id)}>delete</button>
+    </div>
+  {:else}
+    <p>There are no people to show...</p>
+  {/each}
 </main>
